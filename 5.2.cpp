b0ef631947f9beae47ecb54f34e03ace97eef2a5
IvanLyamin <96497489+IvanLyamin@users.noreply.github.com>
2021-12-22 00:49:38 +0300

    Create 5.2.cpp

---
+++ 5.2.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int A = 0;// Задаем координаты точек
+    int B = 4;
+    int C = 10;
+    int AC = C - A;// Производим вычисление
+    int BC = C - B;
+    int summ = AC + BC;
+    printf_s("%i\n%i\n%i", AC, BC, summ);// Выводим на экран полученные даннные
+    return 0;
+}
