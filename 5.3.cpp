3208407e36d52e11ae97cddadc92be8b734e99ac
IvanLyamin <96497489+IvanLyamin@users.noreply.github.com>
2021-12-22 01:45:21 +0300

    Create 5.3.cpp

---
+++ 5.3.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int A = 3;//Задаеи координаты точек
+    int B = 12;
+    int C = 6;
+    int AC = C - A;//Находим длинны отрезков
+    int BC = B - C;
+    int S = AC * BC;//Находим произведение
+    printf_s("%i\n%i\n%i", AC, BC, S);//Выводим ответ на экран
+    return 0;
+}
