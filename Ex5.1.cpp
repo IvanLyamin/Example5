38f95857f4659884471759dd2d5d9520ad50f274
IvanLyamin <96497489+IvanLyamin@users.noreply.github.com>
2021-12-22 00:49:44 +0300

    Create Ex5.1.cpp

---
+++ Ex5.1.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	float x1 = 6;//Задаем координаты точек
+	float y1 = -1;//Задаем координаты точек
+	float x2 = -2;//Задаем координаты точек
+	float y2 = 5;//Задаем координаты точек
+	float distance;
+	distance = sqrt((pow(x2 - x1, 2)) + (pow(y2 - y1, 2)));//Находим расстояние между точками
+	printf_s("%f", distance);//Выводим ответ на экран
+	return 0;
+
+}
